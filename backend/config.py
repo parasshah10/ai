b60e76d23e0cdb16a6da2dde6676531afed12b9a
parasshah10 <116607050+parasshah10@users.noreply.github.com>
2024-07-18 14:03:46 +0000

    a

--- backend/config.py
+++ backend/config.py
@@ -77,13 +77,13 @@ for source in log_sources:
 
 log.setLevel(SRC_LOG_LEVELS["CONFIG"])
 
-WEBUI_NAME = os.environ.get("WEBUI_NAME", "Open WebUI")
-if WEBUI_NAME != "Open WebUI":
-    WEBUI_NAME += " (Open WebUI)"
+WEBUI_NAME = os.environ.get("WEBUI_NAME", "Chat")
+# if WEBUI_NAME != "Open WebUI":
+#     WEBUI_NAME += " (Open WebUI)"
 
 WEBUI_URL = os.environ.get("WEBUI_URL", "http://localhost:3000")
 
-WEBUI_FAVICON_URL = "https://openwebui.com/favicon.png"
+WEBUI_FAVICON_URL = "https://i.ibb.co/PYgRWdH/official-siri-icon-macos-sierra-by-tracedesign-da7cihj-pre-1.png"
 
 
 ####################################
